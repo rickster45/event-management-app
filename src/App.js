fa33eba28f88bec1ed9a3043eb86fd5d4f0316e4
Doucheyyy <80516766+rickster45@users.noreply.github.com>
2026-01-20 23:14:27 -0600

    Add basic React component for the Event Management App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Event Management App</h1>
+            <p>Welcome to the best way to manage your events!</p>
+        </div>
+    );
+};
+
+export default App;
